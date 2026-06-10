29e4a61e4560ea82180614e34f55378eb89f6c59
Viswanath N <viswa0894@gmail.com>
2026-06-10 11:49:35 +0530

    freeRTOS init projects

---
+++ FreeRTOS_MessageQueue/Debug/Startup/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (14.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_SRCS += \
+../Startup/startup_stm32f446retx.s 
+
+OBJS += \
+./Startup/startup_stm32f446retx.o 
+
+S_DEPS += \
+./Startup/startup_stm32f446retx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Startup/%.o: ../Startup/%.s Startup/subdir.mk
+	arm-none-eabi-gcc -mcpu=cortex-m4 -g3 -DDEBUG -c -I../Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2 -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
+
+clean: clean-Startup
+
+clean-Startup:
+	-$(RM) ./Startup/startup_stm32f446retx.d ./Startup/startup_stm32f446retx.o
+
+.PHONY: clean-Startup
+
